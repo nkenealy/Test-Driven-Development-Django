30bb75f30a8b31ef8313dc051da3e933b24c9885
nkenealy <neilkenealy>
2014-12-08 11:36:42 +0000

    initial commit of the chapter 6 code downloaded from hwp Percival git store

---
+++ superlists/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/', include('lists.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
+)

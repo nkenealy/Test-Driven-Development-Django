3993cbb20bf503aaba15e0e7ecb8d85e78d51061
nkenealy <neilkenealy>
2014-12-18 07:04:35 +0000

    first-cut FT

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,20 +1,33 @@
 from .base import FunctionalTest
 from unittest import skip
 
+
 class ItemValidationTest(FunctionalTest):
-
     def test_cannot_add_empty_list_items(self):
-    # Edith goes to the home page and accidentally tries to submit
-    # an empty list item. She hits Enter on the empty input box
 
-    # The home page refreshes, and there is an error message saying
-    # that list items cannot be blank
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
-    # She tries again with some text for the item, which now works
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
-    # Perversely, she now decides to submit a second blank list item
+        # She tries again with some text for the item, which now works
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.check_for_row_in_list_table('1: Buy Milk')
 
-    # She receives a similar warning on the list page
+        # Perversely, she now decides to submit a second blank list item
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
-    # And she can correct it by filling some text in
-        self.fail('write me!')
+        # She receives a similar warning on the list page
+        self.check_for_row_in_list_table('1: Buy Milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+
+        # And she can correct it by filling some text in
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy Milk')
+        self.check_for_row_in_list_table('2: Make Tea')

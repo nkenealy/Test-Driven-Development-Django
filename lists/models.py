ef9b12b6543c0cdd2451462142f0547836feff1b
nkenealy <neilkenealy>
2014-12-22 07:08:53 +0000

    new form for list items

--- lists/models.py
+++ lists/models.py
@@ -7,8 +7,6 @@ class List(models.Model):
         return reverse('view_list', args=[self.id])
 
 
-
-
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)

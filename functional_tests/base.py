d0be4297eb4a01df7dad47e33be27e320f166e80
nkenealy <neilkenealy>
2014-12-29 16:08:27 +0000

    placeholder test_my_lists and move login checkers into base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 import sys
+from selenium.webdriver.support.ui import WebDriverWait
 
 class FunctionalTest(StaticLiveServerTestCase):
 
@@ -32,3 +33,22 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
+
+
+    def wait_for_element_with_id(self, element_id):
+        WebDriverWait(self.browser, timeout=30).until(
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was:\n{}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+            )
+        )
+
+    def wait_to_be_logged_in(self, email):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+    def wait_to_be_logged_out(self, email):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,7 +1,8 @@
 import time
-from selenium.webdriver.support.ui import WebDriverWait
+
 
 from .base import FunctionalTest
+TEST_EMAIL = 'edith@mockmyid.com'
 
 
 class LoginTest(FunctionalTest):
@@ -18,14 +19,6 @@ class LoginTest(FunctionalTest):
         self.fail('could not find window')
 
 
-    def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id),
-            'Could not find element with id {}. Page text was:\n{}'.format(
-                element_id, self.browser.find_element_by_tag_name('body').text
-            )
-        )
-
 
     def test_login_with_persona(self):
         # Edith goes to the awesome superlists site
@@ -40,10 +33,24 @@ class LoginTest(FunctionalTest):
         ## Use mockmyid.com for test email
         self.browser.find_element_by_id(
             'authentication_email'
-        ).send_keys('edith@mockmyid.com')
+        ).send_keys(TEST_EMAIL)
         self.browser.find_element_by_tag_name('button').click()
 
         # The Persona window closes
         self.switch_to_new_window('To-Do')
 
-   
+        # She can see that she is logged in
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
+
+        # Refreshing the page, she sees it's a real session login,
+        # not just a one-off for that page
+        self.browser.refresh()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
+
+        # Terrified of this new feature, she reflexively clicks "logout"
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
+
+        # The "logged out" status also persists after a refresh
+        self.browser.refresh()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)

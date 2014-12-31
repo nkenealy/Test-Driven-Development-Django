3092f4d0bc8d9d304a1016ea42813e6ebbdab067
nkenealy <neilkenealy>
2014-12-31 10:42:03 +0000

    changes to authentication and urls

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -2,16 +2,16 @@ import requests
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-
 PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
 DOMAIN = 'localhost'
 
 
 class PersonaAuthenticationBackend(object):
+
     def authenticate(self, assertion):
         response = requests.post(
             PERSONA_VERIFY_URL,
-            data={'assertion': 'an assertion', 'audience': DOMAIN}
+            data={'assertion': assertion, 'audience': DOMAIN}
         )
         if response.ok and response.json()['status'] == 'okay':
             email = response.json()['email']
@@ -20,7 +20,8 @@ class PersonaAuthenticationBackend(object):
             except User.DoesNotExist:
                 return User.objects.create(email=email)
 
-    def get_user(self,email):
+
+    def get_user(self, email):
         try:
             return User.objects.get(email=email)
         except User.DoesNotExist:
--- superlists/settings.py
+++ superlists/settings.py
@@ -94,3 +94,20 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'superlists', 'static'),
 )
 
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}

1ffbb501a1010df6e8905d3285ce8b1e480e157a
nkenealy <neilkenealy>
2014-12-31 14:57:36 +0000

    put the functional tests back right again

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -22,16 +22,3 @@ class MyListsTest(FunctionalTest):
             value=session.session_key, #2
             path='/',
         ))
-
-
-    def test_logged_in_users_lists_are_saved_as_my_lists(self):
-        email = 'edith@example.com'
-
-        self.browser.get(self.server_url)
-        self.wait_to_be_logged_out(email)
-
-        # Edith is a logged-in user
-        self.create_pre_authenticated_session(email)
-
-        self.browser.get(self.server_url)
-        self.wait_to_be_logged_in(email)
